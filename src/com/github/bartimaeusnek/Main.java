dd61ead31e160a10cb9d654eaebecc14f146e34d
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2019-04-22 17:06:56 +0200

    redid the writer... its better now
    Signed-off-by: bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>

--- src/com/github/bartimaeusnek/Main.java
+++ src/com/github/bartimaeusnek/Main.java
@@ -1,13 +1,11 @@
-/**
- * 
- */
 package com.github.bartimaeusnek;
 
 import java.io.DataInputStream;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 /**
  * @author bartimaeusnek
@@ -16,34 +14,28 @@ import java.util.ArrayList;
 public class Main {
 
 	/**
-	 * @param args[0] input mca file, args[1] output files, will get set automatically (32x32 = 4096)
+	 * @param args input mca file, args[1] output files, will get set automatically (32x32 = 4096)
 	 */
 	public static void main(String[] args) {
 		File f = new File(args[0]);
 		RegionFile minecraft_regionfile = new RegionFile(f);
 		DataInputStream raw_data = null;
 		File[][] output = new File[32][32];
-		
+
 		for (int x = 0; x < 32; x++) {
-		for(int y = 0; y < 32; y++) {
-			output[x][y] = new File(args[1]+"_"+x+"_"+y);
-		try {
-			FileWriter w = new FileWriter(output[x][y]);
-			ArrayList<Integer> buffer = new ArrayList<Integer>();
-				raw_data = minecraft_regionfile.getChunkDataInputStream(x,y);
-				if(raw_data!=null) {
-				do {
-				buffer.add(raw_data.read());
-				}while(buffer.get(buffer.size()-1) != -1);
-			for(int z = 0; z < buffer.size(); z++) 
-			w.write(buffer.get(z));
-			w.close();
+			for(int y = 0; y < 32; y++) {
+				System.out.println("Processing Chunk: " + x + " " + y);
+				output[x][y] = new File(args[1]+"_"+x+"_"+y);
+				output[0][0].getParentFile().mkdirs();
+				try {
+					raw_data = minecraft_regionfile.getChunkDataInputStream(x,y);
+					if (raw_data != null)
+						Files.copy(raw_data,output[x][y].toPath(), StandardCopyOption.REPLACE_EXISTING);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		}
+			}
 		}
 	}
 }
